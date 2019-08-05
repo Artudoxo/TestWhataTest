f6026aa08735b6bd602be1a378a2ad93c1a3e73a
Arturo <arturo.ortueta@gmail.com>
2019-08-04 21:33:25 -0500

    Modificada la pantalla inicial y agregada la Pantalla de Status, empezar a trabajar con firebase.

---
+++ app/src/main/java/com/liarkat/testwhatatest/Status.java
@@ -0,0 +1,14 @@
+package com.liarkat.testwhatatest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Status extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+    }
+}
