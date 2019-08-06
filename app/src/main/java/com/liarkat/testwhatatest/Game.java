0e50a745b45cbbfb60796536b1ae36e1c018b200
Artudoxo <arturo.ortueta@gmail.com>
2019-08-06 13:23:17 -0500

    Se inicio la implementacion del login con Firebase.

---
+++ app/src/main/java/com/liarkat/testwhatatest/Game.java
@@ -0,0 +1,14 @@
+package com.liarkat.testwhatatest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
